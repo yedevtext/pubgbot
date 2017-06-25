a0aca048527dfc1bf5b2c1b4957036d0583da236
Yashar Esfanaji <YasharEsfanaji@gmail.com>
2017-06-25 13:03:39 -0400

    added pubgbotDb project.... lol oops after putting on github realized the the data store would be needed... lol.

---
+++ pubgbot/pubgbotDb/dbo/Tables/User.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[User] (
+    [Id]      INT            IDENTITY (1, 1) NOT NULL,
+    [Name]    NVARCHAR (150) NULL,
+    [SteamId] NVARCHAR (150) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
